d3d5c951817390daad1e36c37d395de20ee32472
UsmanAJabbar <hi@usmanjabbar.com>
2024-10-25 18:35:38 -0500

    first commit

--- script.py
+++ script.py
@@ -4,10 +4,10 @@ from openai import OpenAI
 COMMAND_KEYWORD = '!allat'
 
 # I know I'm exposing these keys, I couldn't care less
-CLIENT_SECRET = 'Xh6s_QX55KrGf-m8JIXH4WuFIaCsPQ'
-CLIENT_ID = 'IrOzq-JxT2yfxkDs4l_gIg'
-USER_AGENT = 'my_tldr_bot/0.1 by Constant-Ebb-4480'
-# OPEN_API_KEY = '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'
+CLIENT_SECRET = ''
+CLIENT_ID = ''
+USER_AGENT = ''
+OPEN_API_KEY = ''
 
 reddit = praw.Reddit(
     client_id=CLIENT_ID,
